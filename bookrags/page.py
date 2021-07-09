a5ed814154f209f49eea4b5cd6fc7f0171aef572
qtkite <kite@uwu.icu>
2021-07-09 19:12:42 +1000

    link setters & getters

--- bookrags/page.py
+++ bookrags/page.py
@@ -1,5 +1,24 @@
 import re
 
+
 class Page:
-    def __init__(self) -> None:
+    def __init__(self, link) -> None:
+        self.__link = link
+
+    def get_link(self):
+        return self.__link
+
+    def set_link(self, link):
+        self.__link = link
+
+    def get_pdf(self, link):
+        """
+        Gets the pdf link from the page
+        """
+        pass
+
+    def get_word(self, link):
+        """
+        Get the word link from the page
+        """
         pass
